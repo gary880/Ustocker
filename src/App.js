d15be63d6506e73c8ffcaa1226a5638f67560d2f
Gary <easycard32145@gmail.com>
2023-01-04 19:18:02 +0800

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from "react";
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/';
+import Tracker from "./components/Tracker";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/home' element={<Home />}></Route>
+      <Route path='/tracker' element={<Tracker />}></Route>
+      <Route path='/navbar' element={<Navbar />}></Route>
+    </Routes >
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom"
+import { Row, Col, Button, Menu } from "antd";
+import styles from "./index.module.css"
+import logoImage from "../../images/logo.png"
+const Navbar = () => {
+    const [current, setCurrent] = useState('mail');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+    const items = [
+        {
+            label: (
+                <Link className={styles.nav_link} to="/home">
+                    產品
+                </Link>
+            ),
+            key: 'home',
+        },
+        {
+            label: (
+                <Link className={styles.nav_link} to="/tracker">
+                    市場
+                </Link>
+            ),
+            key: 'tracker',
+        },
+    ];
+
+
+    return (
+        <>
+            <Row className={styles.navbar}>
+                <Col className={styles.logo} span={4}>
+                    <img className={styles.logo_img} src={logoImage} />
+                </Col>
+                <Col span={16}>
+                    <Menu className={styles.nav_link_wrapper} onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+                </Col>
+                <Col span={4}></Col>
+            </Row>
+        </>
+    )
+
+
+}
+
+
+export default Navbar
